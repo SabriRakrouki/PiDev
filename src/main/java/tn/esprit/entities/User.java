db4e72450e4e84be8066b672673c6bfef7f09d6e
safe <safa.chaouali@esprit.tn>
2022-02-20 00:05:46 +0100

    userr

--- src/main/java/tn/esprit/entities/User.java
+++ src/main/java/tn/esprit/entities/User.java
@@ -1,5 +1,26 @@
 package tn.esprit.entities;
 
-public class User {
-public int a;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public  abstract class User implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private String Photo;
+	private Role role;
+	
+	
+
 }
